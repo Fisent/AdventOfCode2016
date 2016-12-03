01e87b132871e244041d6d4064d4e29482a5cd9d
Fisent <lukzieniewicz@gmail.com>
2016-12-03 23:50:07 +0100

    Day3 - Done

--- Day3/src/day3/Day3.java
+++ Day3/src/day3/Day3.java
@@ -29,21 +29,41 @@ public class Day3
     public static int howManyTraingles() throws FileNotFoundException
     {
         int counter = 0;
-        LinkedList<Integer> list = new LinkedList<>();
+        LinkedList<Integer> list1 = new LinkedList<>();
+        LinkedList<Integer> list2 = new LinkedList<>();
+        LinkedList<Integer> list3 = new LinkedList<>();
         Scanner scan = new Scanner(new FileInputStream("input.txt"));
         while(scan.hasNextLine())
         {
-            String nextLine = scan.nextLine();
+            String nextLine1 = scan.nextLine();
+            String nextLine2 = scan.nextLine();
+            String nextLine3 = scan.nextLine();
             
-            list.clear();
-            if(!nextLine.equals(""))
+            list1.clear();
+            list2.clear();
+            list3.clear();
+            if(true)
             {
-                String[] tab = nextLine.split("\\s+");
-                list.add(Integer.parseInt(tab[1]));
-                list.add(Integer.parseInt(tab[2]));
-                list.add(Integer.parseInt(tab[3]));
-                list.sort(new Comp());
-                if(list.get(0) + list.get(1) > list.get(2))counter++;
+                String[] tab1 = nextLine1.split("\\s+");
+                String[] tab2 = nextLine2.split("\\s+");
+                String[] tab3 = nextLine3.split("\\s+");
+                list1.add(Integer.parseInt(tab1[1]));
+                list1.add(Integer.parseInt(tab2[1]));
+                list1.add(Integer.parseInt(tab3[1]));
+                
+                list2.add(Integer.parseInt(tab1[2]));
+                list2.add(Integer.parseInt(tab2[2]));
+                list2.add(Integer.parseInt(tab3[2]));
+                
+                list3.add(Integer.parseInt(tab1[3]));
+                list3.add(Integer.parseInt(tab2[3]));
+                list3.add(Integer.parseInt(tab3[3]));
+                list1.sort(new Comp());
+                list2.sort(new Comp());
+                list3.sort(new Comp());
+                if(list1.get(0) + list1.get(1) > list1.get(2))counter++;
+                if(list2.get(0) + list2.get(1) > list2.get(2))counter++;
+                if(list3.get(0) + list3.get(1) > list3.get(2))counter++;
             }
         }
         
