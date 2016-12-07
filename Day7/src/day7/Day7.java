c43aba7eb3939e80d7031e4e1bbc69bbc3ceb279
Fisent <lukzieniewicz@gmail.com>
2016-12-07 21:02:43 +0100

    Day7 - First part done

--- Day7/src/day7/Day7.java
+++ Day7/src/day7/Day7.java
@@ -5,6 +5,11 @@
  */
 package day7;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author lukzi_000
@@ -15,16 +20,22 @@ public class Day7
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
-        System.out.println(check("ioxxo"));
+        int i = 0;
+        System.out.println(checkLine("ioxxoj[asdfgh]zxcvbn"));
+        Scanner scan = new Scanner(new FileInputStream("input.txt"));
+        while(scan.hasNextLine())
+        {
+            if(checkLine(scan.nextLine()))i++;
+        }
+        System.out.println(i);
     }
     
     //checks string of four character
     public static boolean checkFourChars(String input)
     {
         char[] tab = input.toCharArray();
-        System.out.println(input);
         return tab[0] == tab[3] && tab[1] == tab[2] && tab[0] != tab[1];
     }
     //checks string of any length
@@ -35,6 +46,51 @@ public class Day7
             if(checkFourChars(input.substring(i, i+4)))return true;
         }
         return false;
+    }
+    
+    public static ArrayList<String> getOutsideBraces(String input)
+    {
+        String s = input.replaceAll("]", "[");
+        String[] in = s.split("\\[");
+        ArrayList<String> out = new ArrayList<String>(); 
+        for(int i = 0; i<in.length;i++)
+        {
+            if(i%2==0) out.add(in[i]);
+                
+        }
+        return out;
+    }
+    
+    public static ArrayList<String> getInsideBraces(String input)
+    {
+        String s = input.replaceAll("]", "[");
+        String[] in = s.split("\\[");
+        ArrayList<String> out = new ArrayList<String>(); 
+        for(int i = 0; i<in.length;i++)
+        {
+            if(i%2==1) out.add(in[i]);
+                
+        }
+        return out;
+    }
+    
+    public static boolean checkLine(String line)
+    {
+        ArrayList<String> outside = getOutsideBraces(line);
+        ArrayList<String> inside = getInsideBraces(line);
+        
+        boolean flag = false;
+        
+        for(String s : outside)
+        {
+            if(check(s)) flag = true;
+        }
+        for(String s : inside)
+        {
+            if(check(s))return false;
+        }
+        
+        return flag;
     }
     
 }
